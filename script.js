393864ebeaf2445924b5f5951c1a6518cd021575
Lase12 <158032462+Lase12@users.noreply.github.com>
2024-02-29 01:20:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let intervalId;
+var daysStr= document.getElementById("days");
+var hoursStr= document.getElementById("hours");
+var minutesStr = document.getElementById("minutes");
+var secondsStr= document.getElementById("seconds");
+
+function startTimer(durationInSeconds) {
+  const timerElement = document.getElementById("timer");
+  let timer = durationInSeconds;
+
+  function updateTimer() {
+    const days = Math.floor(timer / (3600 * 24));
+    const hours = Math.floor((timer % (3600 * 24)) / 3600);
+    const minutes = Math.floor((timer % 3600) / 60);
+    const seconds = timer % 60;
+
+    const daysStr = String(days).padStart(2, "0");
+    const hoursStr = String(hours).padStart(2, "0");
+    const minutesStr = String(minutes).padStart(2, "0");
+    const secondsStr = String(seconds).padStart(2, "0");
+
+    timerElement.textContent =
+      daysStr + "d:" + hoursStr + "h:" + minutesStr + "m:" + secondsStr + "s";
+
+    if (--timer < 0) {
+      clearInterval(intervalId);
+      timerElement.textContent = "00:00:00";
+    }
+  }
+
+  updateTimer();
+  intervalId = setInterval(updateTimer, 1000);
+}
+
+window.onload = function () {
+  const durationInSeconds = 9 * 24 * 3600; // 9 days in seconds
+  startTimer(durationInSeconds);
+};
+
+window.onbeforeunload = function () {
+  clearInterval(intervalId);
+};
